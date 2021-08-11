6dfc27ce0af4cad9ff1649e0357716cb3fc6aa86
unknown <ruth.ps@aguascalientes.tecnm.mx>
2021-08-11 08:35:40 -0500

    primera subida del proyecto

---
+++ src/componentes/Tasks.js
@@ -0,0 +1,15 @@
+import Task from './Task'
+
+const Tasks = (props) =>{
+return(
+  <div>
+  {props.tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+
+  {props.tasks.map((task) => (<Task key={task.id} text={task.text} day={task.day} />))}
+
+  </div>
+)
+}
+export default Tasks
+
+    
